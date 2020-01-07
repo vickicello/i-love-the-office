8d6917b4cfabb578e01a8448d0e5c2a2ad5963da
Vicki Hamm <vhamm@covermymeds.com>
2020-01-07 15:54:03 -0500

    Challenges 1 through 4

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+<div :class="['card', 'img-', 'rounded-0']">
+    <img
+        :src="imgSrc"
+        class="card-img rounded-0"
+        :alt="imgAlt"
+    />
+    <div class="card-body">
+      <slot />
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+      name: 'Card',
+      props: ['imgDirection', 'imgSrc', 'imgAlt']
+    }
+</script>
+
+<style lang="scss">
+
+</style>
